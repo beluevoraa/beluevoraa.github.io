8c07f394a4c3f2f6e15e6cd5d273a1c61d44f2c8
belén <belusolocosasimportantes@gmail.com>
2024-08-27 12:46:11 -0300

    responsive design, menu de hamburguesa, boton de cerrar en galería

--- script.js
+++ script.js
@@ -1,36 +1,10 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('.nav');
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slides");
-    let dots = document.getElementsByClassName("demo");
-    let captionText = document.getElementById("caption");
-
-    // Asegúrate de que el índice de slide esté dentro del rango
-    if (n > slides.length) { slideIndex = 1; }
-    if (n < 1) { slideIndex = slides.length; }
-
-    // Ocultar todos los slides
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    if (menuToggle && nav) {
+        menuToggle.addEventListener('click', function() {
+            nav.classList.toggle('active');
+        });
     }
-
-    // Eliminar la clase activa de todos los dots
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    // Mostrar el slide actual y añadir la clase activa al dot correspondiente
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = slides[slideIndex - 1].getElementsByTagName("img")[0].alt;
-}
+});
